5d0f826b9c54ababe0d74357e1738aa8b1202849
ahe429012-lang <ahe429012@gmail.com>
2026-09-08 17:33:05 -0500

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,24 +1,25 @@
-   <script>
+   
       
             var x;
             var y;
             var ans;
             let timeLeft=60;
+
             var timerDisplay = document.getElementById("timerDisplay");
-            function endQuiz(){
+           
+function endQuiz(){
                 myAns.disabled= true;
                 start===false;
             }
-            var timer = setInterval(function(){
+            
+var timer = setInterval(function(){
                 timeLeft--;
-
     timerDisplay.textContent =
         "Time: " + timeLeft;
 
     if (timeLeft <= 0) {
         endQuiz();
     }
-
 }, 1000);
             
             x=1;
@@ -29,34 +30,50 @@
             var userAnswer = Number(myAns.value);
             var mark = document.getElementById("mark")
             var correct = 0;
-        function start(){
+        
+function start(){
             start = true;
             var questions = 0;
-        }
-            function updateNum(){
+}
+            
+function updateNum(){
                 var qX=document.getElementById("x");
                 var qY=document.getElementById("y");
                 qX.textContent=x;
                 qY.textContent=y;
-        }
-        function problem(){
+}
+
+function problem(){
             x=rand(1,10);
             y=rand(1,10);
-            ans=x*y;
+           
             updateNum();
-           await sleep(15000);
-           if (ans === userAnswer){
+          
+        };
+
+funtion submitNum (){
+
+     ans=x*y;
+      if (ans === userAnswer){
                mark.textContent = "Correct";
                correct++;
            }
            else{
                mark.textContent = "Incorrect";
            }
-           
-        }
+           await sleep(1000);
+            for(questions<15 && start===true; questions++;) 
+            problem();
+            myAns.textContent="";
+  }
+document.addEventListener('keydown', (e) => {
+  if (e.key === 'Enter') { 
+     submitNum():
+  }
+
         
         const button = document.getElementById("start")
-        button.addEventListener("click", start);
+        button.addEventListener("click", start(););
             if(start===true){
               for(questions<15;questions++;) problem();
             }
@@ -64,4 +81,4 @@
             var score = correct/questions;
             var scoreBoard=document.getElementById("score")
             scoreBoard.textContent= score;
-        </script>
+
